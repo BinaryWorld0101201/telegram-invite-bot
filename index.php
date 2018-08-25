7edcf62aac31ec7166b4bd9440bbadc5531f0927
Aman Singh <besoeasy@users.noreply.github.com>
2018-08-25 19:39:06 +0530

    okay
    okay

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+}
+include 'madeline.php';
+
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->start();
+
+$me = $MadelineProto->get_self();
+
+\danog\MadelineProto\Logger::log($me);
+
+if (!$me['bot']) {
+    $MadelineProto->messages->sendMessage(['peer' => '@okayandokay', 'message' => "Hi!\n Testing<3"]);
+
+    try {
+        $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
+    } catch (\danog\MadelineProto\RPCErrorException $e) {
+    }
+
+    $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
+}
+echo 'OK, done!'.PHP_EOL;
+
+?>
