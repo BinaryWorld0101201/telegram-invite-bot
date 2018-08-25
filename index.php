56422cd14270c00d313a9eb1e069775533ae0bc0
Aman Singh <besoeasy@users.noreply.github.com>
2018-08-25 19:56:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,30 +6,22 @@ if (!file_exists('madeline.php')) {
 include 'madeline.php';
 
 $MadelineProto = new \danog\MadelineProto\API('session.madeline');
-$MadelineProto->bot_login('655153898:AAFHsI3oj5ZIed6n-xiEN0NxE5yQ4e5wlHI');
+$MadelineProto->start();
+
 
 $me = $MadelineProto->get_self();
-$id = "carryprotocol";
+$id = "https://t.me/carryprotocol";
 
 
 \danog\MadelineProto\Logger::log($me);
 
 if (!$me['bot']) {
     $MadelineProto->messages->sendMessage(['peer' => '@okayandokay', 'message' => "Hi!\n Testing<3"]);
-
-
+  
     
-    
-    
-    try {
-    $Chat = $MadelineProto->get_pwr_chat($id);
-    } catch (\danog\MadelineProto\RPCErrorException $e) {
-    }
-
- 
+  
 }
 
-echo $Chat;
 echo 'OK, done!'.PHP_EOL;
 
 
